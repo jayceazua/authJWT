84a9c7b876535aee11344cbd1d82f3de31629a43
Jayce Azua <jayce.azua@gmail.com>
2018-10-16 22:24:05 -0400

    added validator

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+let userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: `{VALUE} not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token:{
+            type: String,
+            required: true
+        }
+    }]
+})
+
+
+
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = { User }
