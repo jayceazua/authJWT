722c459b2d33a15b93bcbc5251d44f70933979fe
Jayce Azua <jayce.azua@gmail.com>
2018-11-03 22:26:47 -0700

    hashing my password

--- models/user.js
+++ models/user.js
@@ -80,7 +80,12 @@ UserSchema.pre('save', function (next) {
 
     if(user.isModified('password')) {
         // user.password
-
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            })
+        })
         // user.password = hash
         // next();
     }
